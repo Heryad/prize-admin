820f7561b25694809676a8fb2ee1993407cb11a8
Heryad <heryad.me@gmail.com>
2024-10-10 10:53:58 +0400

    w

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PRIZE-EX",
+  title: "PRIZEX",
   description: "Auth : Heryad S. Mahmood",
 };
 
